3cf6627d4b5ddc4be7189dc20ab058c291a46ac9
Frogmarch <renjie.xu.wuhan@gmail.com>
2015-12-12 16:50:42 +0800

    add setting right bar position synamicly

--- www/js/directives.js
+++ www/js/directives.js
@@ -54,4 +54,17 @@ angular.module('wechat.directives', [])
                 });
             }
         };
-    }]);
+    }])
+    .directive('rjPositionMiddle', ['$window', function($window){
+        return{
+            replace: false,
+            link: function(scope, iElm, iAttrs, controller){
+                var height = $window.innerHeight - 44 - 49 - iElm[0].offsetHeight;
+                if (height >= 0) {
+                    iElm[0].style.top = (height / 2 + 44) + 'px';
+                }else{
+                    iElm[0].style.top = 44 + 'px';
+                }
+            }
+        }
+    }])
